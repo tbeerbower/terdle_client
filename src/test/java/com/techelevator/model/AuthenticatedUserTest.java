deda455fcf7750e10f95f39f5d80a486cb036538
Tom Beerbower <tbeerbower@gmail.com>
2023-11-13 11:20:42 -0500

    add model object unit tests

---
+++ src/test/java/com/techelevator/model/AuthenticatedUserTest.java
@@ -0,0 +1,22 @@
+package com.techelevator.model;
+
+import static org.junit.Assert.*;
+
+public class AuthenticatedUserTest {
+
+    @org.junit.Test
+    public void getToken() {
+    }
+
+    @org.junit.Test
+    public void setToken() {
+    }
+
+    @org.junit.Test
+    public void getUser() {
+    }
+
+    @org.junit.Test
+    public void setUser() {
+    }
+}
